5b626b68d51154c6cceccb1b259e59cfdbea0d62
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2017-04-08 21:54:34 -0400

    Changes to do joystick with websocket

--- core/bus.py
+++ core/bus.py
@@ -3,13 +3,12 @@
 # Description: Bus initializes the port(s) for use, and offers them up as easy
 #	       to access attributes. Additionally, there is the option to reset
 #	       the port(s) by using the restart function.
-
-import serial, time
+import serial
 
 class Bus(object):
-        def __init__(self):
-                self.gamepad = open('/dev/input/js0', 'r')
+  def __init__(self):
+    self.gamepad = open('/dev/input/js0', 'r')
 
-        def restart(self):
-		self.gamepad.close()
-		self.gamepad = open('/dev/input/js0', 'r')
+  def restart(self):
+    self.gamepad.close()
+    self.gamepad = open('/dev/input/js0', 'r')
--- parser_main.py
+++ parser_main.py
@@ -1,17 +1,17 @@
 ############ Logitech F310 Gamepad Controller Main - parser_main.py ##########
 # Original Author: John Zeller
 # Description: Parser_Main polls the data in a python dictionary to check the
-#			   states of several buttons coming from parser_core.py. Once it
-#			   reads these states, it will display them for reference on the
-#			   terminal it was launched in
+#         states of several buttons coming from parser_core.py. Once it
+#         reads these states, it will display them for reference on the
+#         terminal it was launched in
 
 ### NOTES #####################################################################
 # 1) LEAVE MODE 'OFF' there is no support in parser_main.py for MODE ON
 # 2) Naturally the gamepad sends the following values:
-# 	LJ/RJ - Down: 0-127
-# 	LJ/RJ - Up: 255-128
-# 	LJ/RJ - Left: 255-128
-# 	LJ/RJ - Right: 0-127
+#   LJ/RJ - Down: 0-127
+#   LJ/RJ - Up: 255-128
+#   LJ/RJ - Left: 255-128
+#   LJ/RJ - Right: 0-127
 ###############################################################################
 
 ### Buttons/Joys Represented: #################################################
@@ -24,42 +24,47 @@ sys.path.append("./core")
 import threading
 from bus import *
 from parser_core import *
-from gui_main import *
-import Tkinter as tk	# Native Python GUI Framework
 import time
+from websocket import create_connection
 
 class ParserMain(threading.Thread):
-	def __init__(self):
-		# Create bus object
-		self.bus = Bus()
-		# Create a dictionary to be used to keep states from joy_core
-		self.states = { 'A':0, 'B':0, 'X':0, 'Y':0,  		   		\
-				'Back':0, 'Start':0, 'Middle':0,		   			\
-				'Left':0, 'Right':0, 'Up':0, 'Down':0, 				\
-				'LB':0, 'RB':0, 'LT':0, 'RT':0,						\
-				'LJ/Button':0, 'RJ/Button':0, 						\
-				'LJ/Left':0, 'LJ/Right':0, 'LJ/Up':0, 'LJ/Down':0, 	\
-				'RJ/Left':0, 'RJ/Right':0, 'RJ/Up':0, 'RJ/Down':0,	\
-				'Byte0':0, 'Byte1':0, 'Byte2':0, 'Byte3':0,			\
-				'Byte4':0, 'Byte5':0, 'Byte6':0, 'Byte7':0,			\
-				'Byte0/INT':0, 'Byte1/INT':0, 'Byte2/INT':0, 		\
-				'Byte3/INT':0, 'Byte4/INT':0, 'Byte5/INT':0, 		\
-				'Byte6/INT':0, 'Byte7/INT':0}
-		# Launch Parser_Core as a seperate thread to parse the gamepad
-		self.parsercore = ParserCore(self.bus, self.states)
-		self.parsercore.start()
-	
-	def run(self):
-		# Description: Polls the gamepad states and displays their current
-		#			   values on a simple Tkinter GUI
-		
-		# Launch simple GUI with Tkinter (Native GUI on Python)
-		gui = GUI(self.states)     # Create GUI object
-		try:
-			gui.mainloop()  # Launch GUI object into the main loop
-		except AttributeError as ex: # If mainloop is broken then sys.quit
-			print "Found AttributeError, closing debugger"
-			# Now Exit
+  def __init__(self):
+    # Create bus object
+    self.bus = Bus()
+    # Create a dictionary to be used to keep states from joy_core
+    self.states = { 'A':0, 'B':0, 'X':0, 'Y':0,             \
+        'Back':0, 'Start':0, 'Middle':0,             \
+        'Left':0, 'Right':0, 'Up':0, 'Down':0,         \
+        'LB':0, 'RB':0, 'LT':0, 'RT':0,            \
+        'LJ/Button':0, 'RJ/Button':0,             \
+        'LJ/Left':0, 'LJ/Right':0, 'LJ/Up':0, 'LJ/Down':0,   \
+        'RJ/Left':0, 'RJ/Right':0, 'RJ/Up':0, 'RJ/Down':0,  \
+        'Byte0':0, 'Byte1':0, 'Byte2':0, 'Byte3':0,      \
+        'Byte4':0, 'Byte5':0, 'Byte6':0, 'Byte7':0,      \
+        'Byte0/INT':0, 'Byte1/INT':0, 'Byte2/INT':0,     \
+        'Byte3/INT':0, 'Byte4/INT':0, 'Byte5/INT':0,     \
+        'Byte6/INT':0, 'Byte7/INT':0}
+    # Launch Parser_Core as a seperate thread to parse the gamepad
+    self.parsercore = ParserCore(self.bus, self.states)
+    self.ws = create_connection("ws://" + sys.argv[1] + ":13000")
+
+  def run(self):
+    # Description: Polls the gamepad states and displays their current
+    #         values on a simple Tkinter GUI
+
+    # Launch simple GUI with Tkinter (Native GUI on Python)
+    while True:
+      self.parsercore.run()
+      self.ws.send(self.create_msg())
+      print(self.create_msg())
+
+  def create_msg(self):
+    rudder = self.states['LJ/Left'] + self.states['LJ/Right']
+    rudder = rudder / 127.
+    winch = self.states['RJ/Left'] + self.states['RJ/Right']
+    winch = 0 * winch * 6. / 127.
+    string = '{"manual_sail_cmd":{"voltage":%f},"manual_rudder_cmd":{"pos":%f}}' % (winch, rudder)
+    return string
 
 if __name__ == '__main__':
         parser = ParserMain()
